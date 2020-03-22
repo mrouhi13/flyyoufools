41739e0c3d2239ebde1010f8feda36b10bb6b0db
Majid Rouhi <mrouhi13@gmail.com>
2020-03-22 11:29:33 +0430

    Add stepReducer.js

---
+++ src/reducers/stepReducer.js
@@ -0,0 +1,25 @@
+export const stepActions = {
+    UPDATE_CURRENT_STEP: 'UPDATE_CURRENT_STEP',
+    SET_APPROPRIATE_LICENSE: 'SET_APPROPRIATE_LICENSE',
+    RESET_STATE: 'RESET_STATE',
+    IS_LOADING: 'IS_LOADING'
+}
+
+export const stepReducer = (state, action) => {
+    switch (action.type) {
+        case stepActions.UPDATE_CURRENT_STEP:
+            return {
+                ...state,
+                currentStep: action.payload,
+                appropriateLicense: null
+            }
+        case stepActions.SET_APPROPRIATE_LICENSE:
+            return {...state, appropriateLicense: action.payload}
+        case stepActions.RESET_STATE:
+            return {currentStep: action.payload, appropriateLicense: null}
+        case stepActions.IS_LOADING:
+            return {...state, isLoading: action.payload}
+        default:
+            return state
+    }
+}
