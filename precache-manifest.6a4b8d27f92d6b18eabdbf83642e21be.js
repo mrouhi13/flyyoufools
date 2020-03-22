3232e93a177ab361c45cc364e84edb2ce13d0941
Majid Rouhi <mrouhi13@gmail.com>
2020-03-22 18:45:28 +0430

    Updates

---
+++ precache-manifest.6a4b8d27f92d6b18eabdbf83642e21be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b9b05dbb90c3606e5393e44efb7d05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cffe3c7e6f1398d7634",
+    "url": "/static/css/2.bf55bc27.chunk.css"
+  },
+  {
+    "revision": "7e9c3b51dea236c187be",
+    "url": "/static/css/main.89976e5a.chunk.css"
+  },
+  {
+    "revision": "8cffe3c7e6f1398d7634",
+    "url": "/static/js/2.a0ad0579.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.a0ad0579.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e9c3b51dea236c187be",
+    "url": "/static/js/main.59512a13.chunk.js"
+  },
+  {
+    "revision": "1592029b348566919c3e",
+    "url": "/static/js/runtime-main.01d7910d.js"
+  }
+]);
