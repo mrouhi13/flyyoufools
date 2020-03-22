82d60f0757a70c2ed555660241ae74179f998fe5
Majid Rouhi <mrouhi13@gmail.com>
2020-03-22 11:28:22 +0430

    Add constants.js

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+import Steps from './steps'
+import Licenses from './licenses'
+
+export const STEPS = Steps
+export const LICENSES = Licenses
+export const APP_LOGO = 'logo.svg'
+export const BTC_WALLET_ADDRESS = '3PHtQj4QJGPf3fKSzxfThcuUKqf9DaWWoz'
+export const ETH_WALLET_ADDRESS = '0xEd52935dEb1e9e316DC7df06D4705Bc86186ac2C'
